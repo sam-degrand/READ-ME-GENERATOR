0c2efc06ec4b933cc6fdd9fe1bb2433c45d082b2
Sam DeGrandchamp <degrandchamps@gmail.com>
2023-06-05 18:58:20 -0400

    Created questions for readme

--- Develop/index.js
+++ Develop/index.js
@@ -2,9 +2,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
 // TODO: Create a function to write README file
 function generateREADME(answers, licenseBadge, licenseNotice) {
     const readmeTemplate = fs.readFileSync('README_template.md', 'utf8');
@@ -24,8 +21,53 @@ function generateREADME(answers, licenseBadge, licenseNotice) {
   
     return readmeContent;
   }
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+// Prompt the user for project information
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'projectTitle',
+      message: "Enter the project's title:",
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter a description of the project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Enter installation instructions:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Enter usage information:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your application:',
+      choices: ['MIT', 'Apache-2.0', 'GPL-3.0'],
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Enter contribution guidelines:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Enter test instructions:',
+    },
+    {
+      type: 'input',
+      name: 'githubUsername',
+      message: "Enter your GitHub username:",
+    },
+    {
+      type: 'input',
+      name: 'emailAddress',
+      message: "Enter your email address:",
+    },
+  ])
