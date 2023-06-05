a816c84b5ac0cf2b0b079a7456c538dad9956bf1
Sam DeGrandchamp <degrandchamps@gmail.com>
2023-06-05 18:54:42 -0400

    created function and included required packages

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,29 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function generateREADME(answers, licenseBadge, licenseNotice) {
+    const readmeTemplate = fs.readFileSync('README_template.md', 'utf8');
+  
+    // Replace placeholders in the template with user input
+    let readmeContent = readmeTemplate
+      .replace('<project_title>', answers.projectTitle)
+      .replace('<description>', answers.description)
+      .replace('<installation>', answers.installation)
+      .replace('<usage>', answers.usage)
+      .replace('<license_badge>', licenseBadge)
+      .replace('<license_notice>', licenseNotice)
+      .replace('<contributing>', answers.contributing)
+      .replace('<tests>', answers.tests)
+      .replace('<github_username>', answers.githubUsername)
+      .replace('<email_address>', answers.emailAddress);
+  
+    return readmeContent;
+  }
 // TODO: Create a function to initialize app
 function init() {}
 
